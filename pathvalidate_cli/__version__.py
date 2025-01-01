7ee3b48c0a2e68736e1a22c579b999945b342b1a
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2025-01-01 12:21:40 +0900

    Update copyright years
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- pathvalidate_cli/__version__.py
+++ pathvalidate_cli/__version__.py
@@ -2,7 +2,7 @@ from typing import Final
 
 
 __author__: Final = "Tsuyoshi Hombashi"
-__copyright__: Final = f"Copyright 2023-2024, {__author__}"
+__copyright__: Final = f"Copyright 2023-2025, {__author__}"
 __license__: Final = "MIT License"
 __version__ = "0.1.0"
 __maintainer__: Final = __author__
