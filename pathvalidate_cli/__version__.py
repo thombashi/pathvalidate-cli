c61bd64ad052f1226a129cd466bbb9dcd08f1aee
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2025-06-15 15:31:21 +0900

    Bump version to 0.2.0
    Signed-off-by: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>

--- pathvalidate_cli/__version__.py
+++ pathvalidate_cli/__version__.py
@@ -4,6 +4,6 @@ from typing import Final
 __author__: Final = "Tsuyoshi Hombashi"
 __copyright__: Final = f"Copyright 2023-2025, {__author__}"
 __license__: Final = "MIT License"
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 __maintainer__: Final = __author__
 __email__: Final = "tsuyoshi.hombashi@gmail.com"
